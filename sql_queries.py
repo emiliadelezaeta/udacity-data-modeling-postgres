29e4fe9f98d51ef584d84899a740d43c41e474ec
Emilia <emilia.delezaeta@keepler.io>
2021-10-08 15:57:08 +0200

    adding changes

--- sql_queries.py
+++ sql_queries.py
@@ -1,20 +1,45 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXIST songplay;"
+user_table_drop = "DROP TABLE IF EXIST user;"
+song_table_drop = "DROP TABLE IF EXIST song;"
+artist_table_drop = "DROP TABLE IF EXIST artist;"
+time_table_drop = "DROP TABLE IF EXIST time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXIST songplay (
+                            songplay_id INT, 
+                            start_time TIMESTAMP,
+                            user_id INT NOT NULL,
+                            level VARCHAR,
+                            song_id INT NOT NULL,
+                            artist_id INT NOT NULL,
+                            session_id INT NOT NULL,
+                            location VARCHAR,
+                            user_agent VARCHAR,
+                            PRIMARY KEY (songplay_id)
+                            );
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXIST users(
+                            user_id INT,
+                            first_name VARCHAR NOT NULL,
+                            last_name VARCHAR NOT NULL,
+                            gender VARCHAR,
+                            level VARCHAR,
+                            PRIMARY KEY(user_id)                    
+                            );
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXIST song(
+                            song_id INT, 
+                            title INT NOT NULL, 
+                            artist_id INT NOT NULL, 
+                            year INT, 
+                            duration NUMERIC,
+                            PRIMARY KEY (song_id)
+                            );
 """)
 
 artist_table_create = ("""
